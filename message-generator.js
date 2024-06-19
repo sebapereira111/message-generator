8b9936e1e04235ee502acdbdc204b5fd2d6da314
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-06-19 15:33:24 -0400

    FIrst commit

---
+++ message-generator.js
@@ -0,0 +1,9 @@
+// Inspirational Message Generator
+
+// To form the message > 'Embrace ' + message[0][X] + ', ' + message[1][X] + ' and ' + message[2][X]
+const message = [
+    ["challenges", "growth", "change", "uncertainty", "opportunities"], 
+    ["believe in yourself", "stay positive", "persist with passion", "dream big", "celebrate your journey"], 
+    ["spread kindness", "make a difference", "inspire others", "lead by example", "live with gratitude"]
+];
+
