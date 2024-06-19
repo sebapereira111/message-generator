31b22cda8b4c734bf8cc246d40f0a1cecb987ef6
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-06-19 16:04:01 -0400

    First functioning version

--- message-generator.js
+++ message-generator.js
@@ -1,9 +1,18 @@
 // Inspirational Message Generator
 
+// Array containing 3 lists of 5 phrases each
 // To form the message > 'Embrace ' + message[0][X] + ', ' + message[1][X] + ' and ' + message[2][X]
 const message = [
+    ["Accept", "Welcome", "Adopt", "Embody", "Encourage"], 
     ["challenges", "growth", "change", "uncertainty", "opportunities"], 
     ["believe in yourself", "stay positive", "persist with passion", "dream big", "celebrate your journey"], 
     ["spread kindness", "make a difference", "inspire others", "lead by example", "live with gratitude"]
 ];
 
+// Random number (0-4) generator
+const rnd = () => {
+    return Math.floor(Math.random() * 5);
+}
+
+// Printing the inspirational message
+console.log(message[0][rnd()] + ' ' + message[1][rnd()] + ', ' + message[2][rnd()] + ' and ' + message[3][rnd()]);
